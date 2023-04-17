615c9aa9157acf417753f48b7663341e4ae34627
Yasir Sherzad <ys.sherzad@gmail.com>
2023-04-17 22:53:39 +0200

    add loading on startup

--- src/App.js
+++ src/App.js
@@ -6,24 +6,39 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
-  SafeAreaView,
+  View,
+  ActivityIndicator,
   StatusBar,
 } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Home from './home';
 import { theme } from './theme';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true)
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
+  }, [])
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaView style={{ flex: 1, backgroundColor: theme.white }}>
-        <StatusBar barStyle='dark-content' />
-        <Home />
-      </SafeAreaView>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: theme.white }} edges={['top', 'right', 'left']}>
+          <StatusBar barStyle='dark-content' />
+          {isLoading && (
+            <View style={{flex:1, justifyContent: "center", alignItems: 'center'}}>
+              <ActivityIndicator size={'small'} color='#000' />
+            </View>
+          )}
+          {!isLoading && (<Home />)}
+          </SafeAreaView>
+        </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 };
